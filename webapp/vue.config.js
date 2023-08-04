d86dc1aaf197051b9c5ab0178208cf53903833b8
tfuru <t_furu@tf-web.jp>
2023-08-05 02:36:00 +0900

    publicPath 追加

--- webapp/vue.config.js
+++ webapp/vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: './',
+  pages: {
+    index: {
+      entry: 'src/main.ts',
+      title: 'vrma-loader-sample',
+    }
+  }  
 })
